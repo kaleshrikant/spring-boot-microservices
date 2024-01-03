a771f6d6d4d24b5ae001f0441a05b4d5cbfc28e9
Shrikant Kale <kaleshrikant@live.com>
2024-01-03 22:10:37 +0530

    Resilience4J Circuit Breaker

--- Project pTechie/order-service/src/main/java/com/spring/boot/order/controller/OrderController.java
+++ Project pTechie/order-service/src/main/java/com/spring/boot/order/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.spring.boot.order.controller;
 
 import com.spring.boot.order.dto.OrderRequest;
 import com.spring.boot.order.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,8 +16,13 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequest orderRquest) {
-        orderService.placeOrder(orderRquest);
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
         return "Order placed successfully.";
+    }
+
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return "Opps ! Something went wrong, please try to order after some time ..!!!";
     }
 }
